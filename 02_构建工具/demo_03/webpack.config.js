6824f4c4a639371c5341be75e2f98c14219c3c4e
xiongxinyi <1270076754@qq.com>
2022-12-21 16:10:38 +0800

    entry属性

---
+++ 02_构建工具/demo_03/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: "production",
+    // entry: "./hello/hello.js",
+    // entry: ["./src/a.js", "./src/b.js"],
+    entry: {
+        hello: "./src/a.js",
+        b: "./src/b.js"
+    },
+}
