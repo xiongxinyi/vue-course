6b07c3e780135312f8c58694d84c1f9a12dd86fe
xiongxinyi <1270076754@qq.com>
2022-12-21 16:30:58 +0800

    output

--- 02_构建工具/demo_03/webpack.config.js
+++ 02_构建工具/demo_03/webpack.config.js
@@ -1,9 +1,16 @@
+const path = require("path")
 module.exports = {
     mode: "production",
     // entry: "./hello/hello.js",
     // entry: ["./src/a.js", "./src/b.js"],
     entry: {
-        hello: "./src/a.js",
+        a: "./src/a.js",
         b: "./src/b.js"
     },
+    // entry: "./src/a.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name]-[id]-[hash].js",
+        clean: true,
+    }
 }
